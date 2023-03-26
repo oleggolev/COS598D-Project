b684752652fe950f24653ecd882d831f2ac50d50
Oleg Golev <ogolev@princeton.edu>
2023-03-26 16:49:06 -0400

    save and load now works

--- load.py
+++ load.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
 import tensorflow_hub as hub
+import tensorflow as tf
 
 model = hub.load('https://kaggle.com/models/google/humpback-whale/frameworks/TensorFlow2/variations/humpback-whale/versions/1')
-tf.saved_model.save(model, './model.pth')
+tf.saved_model.save(model, "./model")
